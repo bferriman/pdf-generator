61ca4f03b3a96562ac90f5705b0a30235d4b41fe
Ben F <56423163+bferriman@users.noreply.github.com>
2020-01-16 12:52:45 -0500

    got pdf creation working with sample text

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const axios = require("axios");
 const inquirer = require("inquirer");
+const PDFDocument = require("pdfkit");
+const fs = require("fs");
 
-async function getUsername() {
+async function main() {
     try {
         const { username } = await inquirer.prompt({
             message: "GitHub username:",
@@ -11,10 +13,38 @@ async function getUsername() {
         const { data } = await axios.get(
             `http://api.github.com/users/${username}`
         );
+
+        buildPDF(data);
     }
     catch (err) {
         console.log(err);
     }
 }
 
-getUsername();
+const dummyStr = "Well I guess I'm going to come in here and type a bunch of text just to see if anything happens and if I don't quite type enough text to match the lorem ipsum text in the example I guess that's ok but I'll keep typing for a bit just to see if I can get close.";
+
+function buildPDF(user) {
+    const doc = new PDFDocument;
+    doc.pipe(fs.createWriteStream(`${user.name.replace(/ /g, "_")}.pdf`));
+
+    //add content here
+    doc
+    .text('And here is some wrapped text...', 100, 300)
+    .font('Times-Roman', 13)
+    .moveDown()
+    .text(dummyStr, {
+        width: 400,
+        align: 'justify',
+        indent: 30,
+        columns: 2,
+        height: 300,
+        ellipsis: true
+    });
+
+
+    doc.end();
+}
+
+main();
+
+
