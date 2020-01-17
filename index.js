ac4791bd54c07c9b4384be06df7971a11e42410b
Ben F <56423163+bferriman@users.noreply.github.com>
2020-01-17 12:50:20 -0500

    built a lot of the pdf generation logic

--- index.js
+++ index.js
@@ -28,20 +28,64 @@ function buildPDF(user) {
     doc.pipe(fs.createWriteStream(`${user.name.replace(/ /g, "_")}.pdf`));
 
     //add content here
-    doc
-    .text('And here is some wrapped text...', 100, 300)
-    .font('Times-Roman', 13)
+    doc.rect(0, 0, 612, 792)
+    .fill("blue");
+    doc.roundedRect(310, 425, 315, 180, 12)
+    .lineWidth(2)
+    .fillAndStroke("white", "#060666");
+    doc.rect(50, -5, 220, 802)
+    .lineWidth(5)
+    .fillAndStroke("white", "#060666");
+    doc.roundedRect(20, 30, 562, 75, 4)
+    .fillOpacity(0.5)
+    .fill("black");
+    doc.roundedRect(25, 25, 562, 75, 2)
+    .lineWidth(2)
+    .fillOpacity(1)
+    .fillAndStroke("white", "#060666");
+    doc.fontSize(45);
+    doc.fillColor("black")
+    .text(user.name, 50, 40, {
+        width: 512,
+        height: 45,
+        align: "center"
+    });
+    doc.fontSize(28)
+    .text("GitHub Stats", 65, 400, {
+        width: 190,
+        height: 350,
+        align: "center"
+    })
+    .fontSize(20)
     .moveDown()
-    .text(dummyStr, {
-        width: 400,
-        align: 'justify',
-        indent: 30,
-        columns: 2,
-        height: 300,
-        ellipsis: true
+    .text(`Public Repos: ${user.public_repos}`, {
+        align: "left"
+    })
+    .moveDown()
+    .text(`Followers: ${user.followers}`)
+    .moveDown()
+    .text(`Following: ${user.following}`)
+    .moveDown()
+    .text(`Stars: All Em!`);
+
+    doc.roundedRect(310, 145, 275, 210, 12)
+    .lineWidth(2)
+    .fillAndStroke("white", "#060666");
+    doc.fontSize(18)
+    .fillColor("black")
+    .text(user.bio, 335, 170, {
+        width: 225,
+        height: 160,
+        align: "left",
+        oblique: 10
     });
 
 
+    doc.roundedRect(35, 125, 250, 250, 18).clip();
+    doc.image("profilepic.jpg", 35, 125, {fit: [250, 250], align: "center", valign: "center"});
+    doc.roundedRect(35, 125, 250, 250, 18)
+    .lineWidth(10)
+    .stroke("#060666");
     doc.end();
 }
 
